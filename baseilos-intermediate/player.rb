fa0655bea05b19c7f1d98b13f0e617074ae2df26
Jozef Lang <developer@jozeflang.com>
2013-08-09 20:18:43 +0200

    Level 5 solution

--- baseilos-intermediate/player.rb
+++ baseilos-intermediate/player.rb
@@ -1,7 +1,7 @@
 # Ruby warrior player 
 # Author: Jozef Lang <www.jozeflang.com>
 # 
-# Level 4
+# Level 5
 
 class Player
 
@@ -23,8 +23,7 @@ class Player
   		_nearest_enemy = get_nearest_enemy(warrior)
   		warrior.bind!(_nearest_enemy)
   		@bounded_enemies.push(_nearest_enemy)
-  	elsif warrior.health < 20 and warrior.health >= @health
-  		# Rest if took any damage, but if and only if is not under attack
+  	elsif may_rest?(warrior)
   		warrior.rest!
   	elsif @bounded_enemies.size > 0 
   		# Attack bounded enemies nearby
@@ -34,8 +33,14 @@ class Player
   		# All bounded enemies (which looks like captives) are already dead due to previous if branch.
   		warrior.rescue!(get_nearest_captive(warrior))
   	elsif exists_live_units?
+  		_next_direction = warrior.direction_of(@alive_units[0])
   		# If any live unit exists go to it and find out what it is
-  		warrior.walk!(warrior.direction_of(@alive_units[0]))
+  		if stairs_in_the_way?(warrior, _next_direction)
+  			# If there are stairs in the way avoid them for now
+  			warrior.walk!(avoid_direction(warrior, _next_direction))
+  		else 
+  			warrior.walk!(_next_direction)
+  		end
   	else 
   		# If nothing is alive in the room, find the stairs
 	    warrior.walk!(warrior.direction_of_stairs)	
@@ -105,6 +110,64 @@ class Player
   	Returns true if there are any units alive
   	'''
   	return @alive_units.size > 0
+  end
+
+  def exists_live_enemy?()
+  	'''
+  	Returns true if live enemy exists.
+  	Live enemy is:
+  	  - Sludge
+  	  - Captivated (bounded) sludge 
+  	'''
+  	return (@bounded_enemies.size > 0 and @alive_units.any? { |u| u.enemy? })
+  end
+
+  def is_under_attack?(warrior)
+  	'''
+  	Returns true if warrior is under attack.
+  	Warrior is under attack if warrior''s health in the previous turn was higher than current health.
+  	'''
+  	return warrior.health < @health
+  end
+
+  def may_rest?(warrior)
+  	'''
+  	Returns true if warrior may have rest.
+  	Warrior may not have rest if:
+  	  - Is at full health
+  	  - The enemy is nearby
+  	  - Is under attack
+  	  - No enemies are alive 
+  	'''
+  	return (warrior.health < 20 and # Is at full health
+  			is_enemy_near?(warrior) == false and
+  			is_under_attack?(warrior) == false and
+  			exists_live_enemy?() == false)
+  end
+
+  def stairs_in_the_way?(warrior, direction)
+  	'''
+  	Returns flag whether stairs are in the way (direction we want to go)
+  	'''
+  	return warrior.feel(direction).stairs?
+  end
+
+  def is_direction_empty?(warrior, direction)
+  	'''
+  	Returns flag whether wall is in the way (direction we want to go)
+  	'''
+  	return warrior.feel(direction).empty?
+  end
+
+  def avoid_direction(warrior, direction)
+  	'''
+  	Returns an alternative direction to avoid current direction
+  	'''
+  	if [:forward, :backward].include? direction
+  		return [:left, :right].reject! { |d| is_direction_empty?(warrior, d) == false }.sample
+  	else
+  		return [:forward, :backward].reject! { |d| is_direction_empty?(warrior, d) == is_direction_empty? }.sample
+  	end
   end
 
 end
